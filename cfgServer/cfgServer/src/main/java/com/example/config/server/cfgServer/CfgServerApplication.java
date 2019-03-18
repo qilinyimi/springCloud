877ca3958ed7689b63b4e26500c7f1a642053420
DESKTOP-6VJT9G4\kyyi <zhanglianbao@joyschool.cn>
2019-03-18 18:02:37 +0800

    add config server

---
+++ cfgServer/cfgServer/src/main/java/com/example/config/server/cfgServer/CfgServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.config.server.cfgServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfgServerApplication.class, args);
+	}
+
+}

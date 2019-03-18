b30b7af6658857da3026dce601f252303b3e6503
DESKTOP-6VJT9G4\kyyi <zhanglianbao@joyschool.cn>
2019-03-18 17:34:15 +0800

    spring cloud config client init

---
+++ cfgClient/src/main/java/com/example/config/client/cfgClient/CfgClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.config.client.cfgClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfgClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfgClientApplication.class, args);
+	}
+
+}

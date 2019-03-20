b6ad7e57b39b724219f1b337ba16b5a162fb7216
DESKTOP-6VJT9G4\kyyi <zhanglianbao@joyschool.cn>
2019-03-20 15:24:59 +0800

    modify name

---
+++ eurekaServer_8761/src/main/java/com/example/eureka/server/eurekaServer/EurekaServerApplication_8761.java
@@ -0,0 +1,15 @@
+package com.example.eureka.server.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication_8761 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication_8761.class, args);
+	}
+
+}
--- eurekaServer_8762/src/main/java/com/example/eureka/server/eurekaServer/EurekaServerApplication_8762.java
+++ eurekaServer_8762/src/main/java/com/example/eureka/server/eurekaServer/EurekaServerApplication_8762.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServerApplication_8762 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(EurekaServerApplication_8762.class, args);
 	}
 
 }
